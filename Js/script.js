035e9a567311d1d519f9b0060b75089c0c598265
Trung <thanhtrung3802@gmail.com>
2023-11-10 23:16:34 +0700

    11:16
    Signed-off-by: Trung <thanhtrung3802@gmail.com>

---
+++ Js/script.js
@@ -0,0 +1,40 @@
+// nút chuyển trang lên đầu
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollToTopBtn").style.display = "none";
+    }
+} // hiển thị ra nút
+
+function scrollToTop() {
+    const scrollDuration = 1000; // Thời gian để cuộn lên (1 giây)
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+
+    const scrollInterval = setInterval(function() {
+        if (window.scrollY != 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 15);
+}
+// tìm kiếm 
+
+function toggleSearch() {
+    var searchBox = document.getElementById("search-box");
+    if (searchBox.style.display === "none") {
+        searchBox.style.display = "block";
+    } else {
+        searchBox.style.display = "none";
+    }
+}
+
+function performSearch() {
+    // Thực hiện tìm kiếm ở đây
+    alert("Đã tìm kiếm: " + document.querySelector("#search-box input").value);
+}
